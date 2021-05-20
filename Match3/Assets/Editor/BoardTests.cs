49de73d8274ddbfde16aeb50fede83c719e395ea
Rodrigo Malhães Bukowitz <rodrigomalhaes@gmail.com>
2021-05-19 21:40:27 -0300

    CheckMatch3 in Model and Test

--- Match3/Assets/Editor/BoardTests.cs
+++ Match3/Assets/Editor/BoardTests.cs
@@ -16,11 +16,50 @@ public class BoardTests
       // VERIFY BOUNDS OF BOARD 
       Assert.That(board.rows, Is.EqualTo(10));
       Assert.That(board.colls, Is.EqualTo(5));
+      
+      // verify if its possible to create the board 
+      Board brokenBoard = new Board(2, 10, 5);
+      Assert.That(brokenBoard.BoardPieces, Is.EqualTo(null));
    }
 
+   [Test]
+   public void CheckMatch3_Test()
+   {
+      Board board = new Board(3, 10, 5);
+      // PrintBoard(board);
+      // Dont match3 test 
+      Assert.That(board.CheckMatch3(), Is.EqualTo(null));
+      
+      // Horizontal test 
+      int[,] testA =  { { 1, 1, 1 }, { 3, 4, 2 }, { 5, 6, 9 }, { 7, 8, 9 } };
+      board.BoardPieces = testA;
+      board.colls = 3;
+      board.rows = 4;
+      List<Point> points = new List<Point>();
+      points.Add(new Point(0,0));
+      points.Add(new Point(0,1));
+      points.Add(new Point(0,2));
+      // PrintBoard(board); // debug option to show the board 
+      Assert.That(board.CheckMatch3(), Is.EqualTo(points));
+      
+      // Vertical test
+      // Horizontal test 
+      int[,] testB = { { 1, 2, 1 }, { 1, 4, 2 }, { 1, 6, 9 }, { 7, 8, 9 } };
+      board.BoardPieces = testB;
+      board.colls = 3;
+      board.rows = 4;
+      List<Point> pointsB = new List<Point>();
+      pointsB.Add(new Point(0,0));
+      pointsB.Add(new Point(1,0));
+      pointsB.Add(new Point(2,0));
+      // PrintBoard(board); // debug option to show the board 
+      Assert.That(board.CheckMatch3(), Is.EqualTo(pointsB));
+   }
+
+   // HELPER FUNCTION TO SHOW THE BOARD IN CONSOLE 
    private void PrintBoard(Board board)
    {
-      for (int i = 0; i < board.rows; i++)
+      for (int i = board.rows - 1; i >= 0; i--)
       {
          List<int> row = new List<int>() ;
          for (int j = 0; j < board.colls; j++)
--- Match3/Assets/Scripts/Board.cs
+++ Match3/Assets/Scripts/Board.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+public struct Point
+{
+    public int row;
+    public int coll;
+
+    public Point(int row, int coll)
+    {
+        this.row = row;
+        this.coll = coll;
+    }
+}
+
 public class Board
 {
     public int[,] BoardPieces; // model of the game board 
@@ -20,9 +32,17 @@ public class Board
 
     void CreateBoard()
     {
+        // check if its possible to create board 
+        if(_numberOfPieces <= 2)
+        {
+            Debug.LogWarning("You cant create an board with only 2 pieces");
+            BoardPieces = null;
+            return;
+        }
+        
         int[,] boardMatrix = new int[rows,colls];
         // variables to avoid starting with a match 
-        int[] previousLeft = new int[10000];
+        int[] previousLeft = new int[rows];
         int previousBelow = 0;
         
         // all pieces representations 
@@ -49,6 +69,59 @@ public class Board
         }
 
         BoardPieces = boardMatrix;
+    }
+
+    // return the list of points of the FIRST match3 found. Return null if NO match3 was found 
+    public List<Point> CheckMatch3()
+    {
+        List<Point> points = new List<Point>();
+       
+        for (int i = 0; i < colls; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                // Can exclude the corners 
+                if ((j == 0 && i == 0) || (j == rows -1 && i == colls-1) || (j == rows-1 && i == 0) || (j == 0 && i == colls-1))
+                    continue;
+                
+                // Check horizontal match 3 
+                if (i != 0 && i != colls - 1)
+                {
+                    int middlePiece = BoardPieces[j, i];
+                    int leftPiece = BoardPieces[j, i - 1];
+                    int rightPiece = BoardPieces[j, i + 1];
+                    
+                    if ( middlePiece == leftPiece && middlePiece == rightPiece)
+                    {
+                        // its a match 3 
+                        points.Add(new Point(j,i-1));
+                        points.Add(new Point(j,i));
+                        points.Add(new Point(j,i + 1));
+                        return points;
+                    }
+                }
+                    
+                
+                // Check vertical match 3 
+                if (j != 0 && j != rows - 1)
+                {
+                    int middlePiece = BoardPieces[j, i];
+                    int upPiece = BoardPieces[j + 1, i];
+                    int downPiece = BoardPieces[j - 1, i];
+
+                    if ( middlePiece == upPiece && middlePiece == downPiece)
+                    {
+                        // its a match 3 
+                        points.Add(new Point(j - 1,i));
+                        points.Add(new Point(j,i));
+                        points.Add(new Point(j + 1,i));
+                        return points;
+                    }
+                }
+            }
+        }
+
+        return null;
     }
     
 }
